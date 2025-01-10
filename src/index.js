26850c4f647ac68c1e52c2350bbd3afa70d43f5c
Arnav-Sangle <arnavsangle7x24@gmail.com>
2025-01-10 23:55:56 +0530

    update root.render() to ReactDOM.render() to match react 17
    Option 2: Modify the Code for React 17
    If you prefer to stick with react@17, you’ll need to adjust your code to avoid using react-dom/client.
    
    Locate where react-dom/client is being imported in your project (probably in index.js or App.js).
    
    Replace the import with import ReactDOM from 'react-dom';
    Replace ReactDOM.createRoot() with ReactDOM.render():

--- src/index.js
+++ src/index.js
@@ -1,39 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; // Modified import for React 17
 import App from "./App";
 
-import {Provider} from "react-redux"
+import { Provider } from "react-redux";
 import store from "./store";
 
 // https://www.npmjs.com/package/react-alert
-import { Provider as AlertProvider, transitions, positions } from "react-alert"
-import AlertTemplate from "react-alert-template-basic"
+import { Provider as AlertProvider, transitions, positions } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
-
-// optional configuration (COPIED from https://www.npmjs.com/package/react-alert)
+// Optional configuration (Copied from https://www.npmjs.com/package/react-alert)
 const options = {
   position: positions.BOTTOM_CENTER,
   timeout: 5000,
   offset: '30px',
   transition: transitions.SCALE
-}
-    
+};
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store = {store}>
-    {/* to connect Store to React app, we use <Provider> */}
-
+ReactDOM.render( // Modified for React 17
+  <Provider store={store}>
+    {/* To connect Store to React app, we use <Provider> */}
     <AlertProvider template={AlertTemplate} {...options}>
-
       {/* <React.StrictMode> */}
       <App />
       {/* </React.StrictMode> */}
-
     </AlertProvider>
-
-  </Provider>
+  </Provider>,
+  document.getElementById("root") // Modified for React 17
 );
 
 // If you want to start measuring performance in your app, pass a function
